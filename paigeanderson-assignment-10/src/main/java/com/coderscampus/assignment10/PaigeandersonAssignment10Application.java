3b65b32ece2d3d7c69678afb5b6d09214eb63c1d
Paige Anderson <paige.m.anderson1999@gmail.com>
2023-06-30 11:33:53 -0700

    initial commit

---
+++ paigeanderson-assignment-10/src/main/java/com/coderscampus/assignment10/PaigeandersonAssignment10Application.java
@@ -0,0 +1,15 @@
+package com.coderscampus.assignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaigeandersonAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaigeandersonAssignment10Application.class, args);
+		
+		//test;
+	}
+
+}
